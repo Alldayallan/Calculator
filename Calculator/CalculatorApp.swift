0f55f4a7398be81c8fdcaec3b3f0edabaaae0330
Alldayallan <mail@allanreid.co>
2025-01-07 17:09:54 -0800

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Allan Reid on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
